856155e4d80c24f3aa2be9c1e723ccfc4f6cfb19
DyatkoGleb <glebikon@yandex.ru>
2024-01-02 16:24:53 +0300

    Feat: Add the functionality to receive notes

--- app/bot.js
+++ app/bot.js
@@ -1,13 +1,49 @@
 const TelegramBotApi = require('node-telegram-bot-api')
 const axios = require('axios')
+const noticerApiUrl = process.env.NOTICER_API_URL
+
+const getMyCommands = () => {
+    return [
+        { command: '/notes', description: 'Получить все заметки' }
+    ]
+}
+
+const makeNotes = (notes) => {
+    const label = 'Заметки\n\n'
+    let notesMessage = '';
+
+    for (let note of notes) {
+        notesMessage += note.text + '\n\n'
+    }
+
+    return label + notesMessage
+}
+
+const getNotes = async () => {
+    const notes = await axios.get(`${noticerApiUrl}/getNotes`)
+        .then(response => response.data.data)
+        .catch(err => err.message)
+
+    return makeNotes(notes)
+}
+
+const messageHandler = async (msg, bot) => {
+    const message = msg.text
+    const chatId = msg.chat.id
+
+    if (message === '/notes') {
+        bot.sendMessage(chatId, await getNotes())
+    } else {
+        axios.post(`${noticerApiUrl}/addNewNote`, { message: msg.text })
+            .catch(err => bot.sendMessage(msg.chat.id, err.message))
+    }
+}
 
 const createBot = () => {
     const bot = new TelegramBotApi(process.env.TG_BOT_TOKEN, { polling: true })
 
-    bot.on('message', msg => {
-        axios.post(`${process.env.NOTICER_API_URL}/addNewNote`, { message: msg.text })
-            .catch(err => bot.sendMessage(msg.chat.id, err.message))
-    })
+    bot.setMyCommands(getMyCommands())
+    bot.on('message',  msg => messageHandler(msg, bot))
 
     return bot
 }
